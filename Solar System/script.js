704344103c9c1159ac7180747769b81c9cff2929
Katelyn <kjcollins7@gmail.com>
2023-02-20 16:09:03 -0500

    solar system created

---
+++ Solar System/script.js
@@ -0,0 +1,12 @@
+
+// Asteroid Belt
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+const STAR_COUNT = 100
+let result = ""
+for(let i = 0; i < STAR_COUNT; i++){
+    result += `${randomNumber(-25, 25)}vw ${randomNumber(-50, 50)}vh ${randomNumber(0, 3)}px ${randomNumber(0,3)}px #59373F,`
+}
+console.log(result.substring(0, result.length - 1))
