ff6a908f940edfa991ca274bf8e7376f87b5eff5
Katelyn <kjcollins7@gmail.com>
2023-02-23 17:55:19 -0500

    randomly generated background

--- script.js
+++ script.js
@@ -1,14 +1,68 @@
-// Asteroid Belt/ Sra Randomizer
-    // function randomNumber(min, max) {
-    //     return Math.floor(Math.random() * (max - min + 1)) + min
-    // }
+  
+// Generate Background Asteroid Belt and Stars
+function generateBackground(){
+  // Star Randomizer
+  function generateStars() {
+    try {const STAR_COUNT = 400
+      let result = ""
+      function randomNumber(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min
+      }
+      function randomStars() {
+        for(let i = 0; i < STAR_COUNT; i++){
+            result += `${randomNumber(-95, 95)}vw ${randomNumber(-95, 95)}vh ${randomNumber(0, 2)}px ${randomNumber(0,2)}px #FFF,`
+        }
+      }
+      randomStars();
+      let stars = result.substring(0, result.length - 1);
+      document.getElementById("stars").style.boxShadow = stars;
+    } catch (error) {
+      // Catch Error Here
+    }
+  }
+  // Generate Asteroid Belts
+  function generateAsteroids1() {
+    try {const Asteroid_COUNT = 50
+      let result = ""
+      function randomNumber(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min
+      }
+      function randomRocks() {
+        for(let i = 0; i < Asteroid_COUNT; i++){
+            result += `${randomNumber(-25, 25)}vw ${randomNumber(-25, 25)}vh ${randomNumber(0, 3)}px ${randomNumber(0,3)}px #301B2F,`
+        }
+      }
+      randomRocks();
+      let rocks = result.substring(0, result.length - 1);
+      document.getElementById("asteroids-belt").style.boxShadow = rocks;
+    } catch (error) {
+      // Catch Error Here
+    }
+  }
+  // Generate Asteroid Belts
+  function generateAsteroids2() {
+    try {const Asteroid_COUNT = 30
+      let result = ""
+      function randomNumber(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min
+      }
+      function randomRocks() {
+        for(let i = 0; i < Asteroid_COUNT; i++){
+            result += `${randomNumber(-30, 30)}vw ${randomNumber(-30, 30)}vh ${randomNumber(0, 2)}px ${randomNumber(0,3)}px #382B2F,`
+        }
+      }
+      randomRocks();
+      let rocks = result.substring(0, result.length - 1);
+      document.getElementById("asteroids-belt2").style.boxShadow = rocks;
+    } catch (error) {
+      // Catch Error Here
+    }
+  }
+  generateStars();
+  generateAsteroids1();
+  generateAsteroids2();
+}
 
-    // const STAR_COUNT = 100
-    // let result = ""
-    // for(let i = 0; i < STAR_COUNT; i++){
-    //     result += `${randomNumber(-25, 25)}vw ${randomNumber(-50, 50)}vh ${randomNumber(0, 3)}px ${randomNumber(0,3)}px #59373F,`
-    // }
-    // console.log(result.substring(0, result.length - 1)
 
 // Close Menu Upon Button Click
 function closeMenu() {
@@ -48,6 +102,7 @@ function closeMenu() {
     const container = document.getElementById("container");
     container.addEventListener('click', openMenu);
     exitButton.addEventListener('click', closeMenu);
+    generateBackground();
   };
 
 // On Load
