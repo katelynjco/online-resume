0e72c4eb6b925e414eb4c679e00c57de44e3390b
Katelyn <kjcollins7@gmail.com>
2023-03-06 17:22:37 -0500

    updated writing content

--- embeded/projects.js
+++ embeded/projects.js
@@ -1,48 +1,48 @@
 const projectArray = [
     {
         "title": "Modern Website Sample",
-        "description": "A sleek and modern site that utlizised a dark mode/light mode theme",
+        "description": "A sleek example product that uses JavaScript to switch between a light and a dark theme",
         "image": "img/projects/modernSample.png",
         "url": "https://katelynjco.github.io/light-dark-mode/",
         "code": "https://github.com/katelynjco/light-dark-mode"
     },
     {
         "title": "Joke Telling Robot",
-        "description": "A robot that uses speech to text to tell jokes",
+        "description": "This robot sources programing jokes from an API, then passes them through a text to speech API using JavaScript",
         "image": "img/projects/jokeTeller.png",
         "url": "https://katelynjco.github.io/joke-teller/",
         "code": "https://github.com/katelynjco/joke-teller"
     },
     {
         "title": "Unsplash API - Infinite Scroll",
-        "description": "A site that auto-poputlates photos for endless scrolling",
+        "description": "Utilizes JavaScript to endlessly scroll through API sourced photos",
         "image": "img/projects/infiniteScroll.png",
         "url": "https://katelynjco.github.io/infinite-scroll/",
         "code": "https://github.com/katelynjco/infinite-scroll"
     },
     {
         "title": "Quote Generator",
-        "description": "Sources quotes from an API and generates them in a pleasing theme",
+        "description": "Sources quotes from an API and generates them in a pleasing theme using JavaScript",
         "image": "img/projects/quoteGenerator.png",
         "url": "https://katelynjco.github.io/quote-generator/",
         "code": "https://github.com/katelynjco/quote-generator"
     },
     {
         "title": "CSS Background Generator",
-        "description": "Generates a gradient background in CSS code",
+        "description": "A CSS code generator that uses JavaScript to assign the background gradient",
         "image": "img/projects/backgroundGenerator.png",
         "url": "https://katelynjco.github.io/background-generator/",
         "code": "https://github.com/katelynjco/background-generator"
     },
     {
         "title": "Picture-in-Picture",
-        "description": "This site allows you to create a picture in picture window of any tab",
+        "description": "This site uses JavaScript to turn any tab into a picture-in-picture window",
         "image": "img/projects/picturePicture.png",
         "url": "https://katelynjco.github.io/picture-in-picture/",
         "code": "https://github.com/katelynjco/picture-in-picture"
     },
     {
-        "title": "Joke Landing Page",
+        "title": "Prank Landing Page",
         "description": "A site generated completely from HTML, designed for a laugh",
         "image": "img/projects/tcTuggers.png",
         "url": "https://katelynjco.github.io/tc-tuggers/",
